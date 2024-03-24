db3c270fb6c7dbbb9a2acbe608e81848416c03c4
MalicknND <ndiayemalick062@gmail.com>
2024-03-24 15:21:43 +0100

    ✅ search fonctionnel

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,16 +3,17 @@ import React, { useEffect, useState } from "react";
 import Card from "./Card";
 const Form = () => {
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("code");
 
   useEffect(() => {
     axios
       .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=1f54bd990f1cdfb230adb312546d765d&query=code&langage=fr-FR`
+        `https://api.themoviedb.org/3/search/movie?api_key=1f54bd990f1cdfb230adb312546d765d&query=${search}&langage=fr-FR`
       )
       .then((res) => {
         setMovies(res.data.results);
       });
-  }, []);
+  }, [search]);
 
   return (
     <div>
@@ -23,6 +24,9 @@ const Form = () => {
               type="text"
               placeholder="Entrez le nom du film"
               id="search-input"
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
             />
             <input type="submit" value="Rechercher" />
           </form>
